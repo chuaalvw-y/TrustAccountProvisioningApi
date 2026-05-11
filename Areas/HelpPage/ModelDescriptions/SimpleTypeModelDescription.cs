5cd73ba25f8d50c4a0ab0707d93295585216d7a8
chuaalvw-y <115362773+chuaalvw-y@users.noreply.github.com>
2026-05-11 09:24:21 -0700

    Base Project

---
+++ Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TrustAccountProvisioningApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TrustAccountProvisioningApi.WebApiApplication" Language="C#" %>
